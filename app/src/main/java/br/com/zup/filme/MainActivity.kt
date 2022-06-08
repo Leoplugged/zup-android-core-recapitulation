acaeecae7e629533688ef066b1ceff83a1cbdcbd
LeoPlugged <leandro.miguel@outlook.com>
2022-06-08 15:46:11 -0300

    Adicionar 1umbotão que leva a outra activity e criaçr activity FilmeDetails.kt

--- app/src/main/java/br/com/zup/filme/MainActivity.kt
+++ app/src/main/java/br/com/zup/filme/MainActivity.kt
@@ -1,11 +1,28 @@
 package br.com.zup.filme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEval = findViewById<Button>(R.id.button_evaluation)
+        buttonEval.setOnClickListener {
+            Toast.makeText(this, "Este filme é inspirador!", Toast.LENGTH_LONG).show()
+        }
+
+        val buttonSynopsis = findViewById<Button>(R.id.button_synopsis)
+        buttonSynopsis.setOnClickListener {
+            val intent = Intent(this, FilmeDetails::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 }
