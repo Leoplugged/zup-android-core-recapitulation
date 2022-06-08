c8b03a79240b98d1cdd04e3a2927812d4a5af358
LeoPlugged <leandro.miguel@outlook.com>
2022-06-08 19:59:49 -0300

    Criar botão de e-mail e estudar Ciclo de Vida

--- app/src/main/java/br/com/zup/filme/FilmeDetails.kt
+++ app/src/main/java/br/com/zup/filme/FilmeDetails.kt
@@ -2,16 +2,18 @@ package br.com.zup.filme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
 
 class FilmeDetails : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(MainActivity.TAG, "Passou pelo onCreate() da FilmeDetails")
 
         setContentView(R.layout.activity_filme_details)
 
-        val bundle = intent.getBundleExtra("Message")
+        val bundle = intent.getBundleExtra("Message")  //TODO Qual a necessidade da "Message"?! Seria o nome da Bundle?
         val nomeDoFilme = bundle!!.getString("nomeDoFilme")
 
         Toast.makeText(this, nomeDoFilme, Toast.LENGTH_LONG).show()
@@ -31,5 +33,35 @@ class FilmeDetails : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity.TAG, "Passou pelo onStart() do FilmeDetails")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity.TAG, "Passou pelo onResume() do FilmeDetails")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity.TAG, "Passou pelo onPause() do FilmeDetails")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity.TAG, "Passou pelo onStop() do FilmeDetails")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity.TAG, "Passou pelo onDestroy() do FilmeDetails")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity.TAG, "Passou pelo onRestart() do FilmeDetails")
     }
 }
--- app/src/main/java/br/com/zup/filme/MainActivity.kt
+++ app/src/main/java/br/com/zup/filme/MainActivity.kt
@@ -3,12 +3,19 @@ package br.com.zup.filme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "APP_FILME"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "Passou pelo onCreate() da MainActivity")
         setContentView(R.layout.activity_main)
 
         val buttonEval = findViewById<Button>(R.id.button_evaluation)
@@ -25,9 +32,48 @@ class MainActivity : AppCompatActivity() {
             val nomeDoFilme = "Captain Fantastic"
             val bundle = Bundle()
             bundle.putString("nomeDoFilme", nomeDoFilme)
-            intent.putExtra("Message", bundle)
-
+            intent.putExtra("Message", bundle) //TODO Qual a necessidade da "Message"?! Seria o nome da Bundle?
             startActivity(intent)
         }
+
+        val buttonMail = findViewById<Button>(R.id.button_mail)
+        buttonMail.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/html"
+            intent.putExtra(Intent.EXTRA_EMAIL, "leandro.almeida@zup.com.br")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "APP do Filme")
+            intent.putExtra(Intent.EXTRA_TEXT, "Olá, Leandro!\n\n Mensagem originada a partir do app FILME:\n\n [digite sua mensagem aqui]")
+            startActivity(intent)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Passou pelo onStart() da MainActivity")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Passou pelo onResume() da MainActivity")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Passou pelo onPause() da MainActivity")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Passou pelo onStop() da MainActivity")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "Passou pelo onDestroy() da MainActivity")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "Passou pelo onRestart() da MainActivity")
     }
 }
